79d49f7f96ae8e3aeb5943683d498f1a377f2d9a
Igor matos ribeiro <igortecnologia2020@gmail.com>
2021-05-07 11:18:54 -0300

    Nomecompleto em java

---
+++ nome_completo.js
@@ -0,0 +1,14 @@
+let nome= prompt("Digite seu nome");
+
+let sobrenome= prompt("Digite seu sobrenome");
+
+let nomecompleto= `Nome Completo: ${nome} ${sobrenome}.`
+
+alert(nomecompleto);
+
+let sobrenome2= sobrenome.toUpperCase()
+
+let nomecompleto2= `Nome de catálogo: ${sobrenome2} ${nome}.`
+
+alert(nomecompleto2);
+
